c22189e537284f0921efa8d88deb602e3ef51043
Vaishnaviverma09 <vverma8084@gmail.com>
2026-07-15 18:20:58 +0530

    hardcoding route

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
 // client/src/config.js
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
-
+/*const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';*/
+const API_BASE_URL = 'https://vaishworld-backend.onrender.com'
 export default API_BASE_URL;
